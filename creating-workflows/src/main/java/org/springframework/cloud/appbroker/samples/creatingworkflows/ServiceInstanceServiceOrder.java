77f0eef1be5a1cb699eeb841e01d4877243114ec
Alberto Rios <arios@pivotal.io>
2020-01-21 11:24:44 +0100

    Adding sample on creating workflows

---
+++ creating-workflows/src/main/java/org/springframework/cloud/appbroker/samples/creatingworkflows/ServiceInstanceServiceOrder.java
@@ -0,0 +1,9 @@
+package org.springframework.cloud.appbroker.samples.creatingworkflows;
+
+public class ServiceInstanceServiceOrder {
+
+	private static final int CREATE_SI_WORKFLOW_ORDER = 0;
+
+	public static final int VALIDATE_CREATE_PARAMETERS = CREATE_SI_WORKFLOW_ORDER - 400;
+
+}
